903c2a30328da5fe5ce7310a299a303ecd273fd6
hyeonorof <hyeonorof@gmail.com>
2022-08-01 22:22:18 +0900

    생성일자 수정일자 자동 반영

--- src/main/java/com/beesket/beesketclone/BeesketCloneApplication.java
+++ src/main/java/com/beesket/beesketclone/BeesketCloneApplication.java
@@ -2,7 +2,9 @@ package com.beesket.beesketclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //생성일자 수정일자 반영
 @SpringBootApplication
 public class BeesketCloneApplication {
 
