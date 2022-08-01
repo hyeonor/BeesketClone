83ecc472c2201c6f5afb9fc4f72359569df4de4b
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-01 21:42:43 +0900

    user 오류 수정

--- src/main/java/com/beesket/beesketclone/model/User.java
+++ src/main/java/com/beesket/beesketclone/model/User.java
@@ -2,15 +2,13 @@ package com.beesket.beesketclone.model;
 
 import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
