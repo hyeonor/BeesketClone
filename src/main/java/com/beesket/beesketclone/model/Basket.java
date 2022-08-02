5331b2803a5d5ed34b83480a629e9c539dda0726
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-02 17:30:20 +0900

    장바구니 1차 완료

--- src/main/java/com/beesket/beesketclone/model/Basket.java
+++ src/main/java/com/beesket/beesketclone/model/Basket.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Basket extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long basketId;
 
     @OneToOne
     @JoinColumn(name = "userId")
