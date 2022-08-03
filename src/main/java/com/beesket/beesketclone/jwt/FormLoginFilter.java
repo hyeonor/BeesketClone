ddd9495c961f8708070644e9d3c6f2858d8af72a
hyeonorof <hyeonorof@gmail.com>
2022-08-03 16:01:15 +0900

    토큰 만료 시간 변경

--- src/main/java/com/beesket/beesketclone/jwt/FormLoginFilter.java
+++ src/main/java/com/beesket/beesketclone/jwt/FormLoginFilter.java
@@ -74,7 +74,7 @@ public class FormLoginFilter extends UsernamePasswordAuthenticationFilter {
         //RSA방식은 아니고 Hash암호 방식
         String jwtToken = JWT.create()
                 .withSubject("cos토큰")
-                .withExpiresAt(new Date(System.currentTimeMillis()+(60000*10)))
+                .withExpiresAt(new Date(System.currentTimeMillis()+(60000*60)))
                 .withClaim("email",userDetails.getUser().getEmail())
                 .sign(Algorithm.HMAC512("cos"));
 
