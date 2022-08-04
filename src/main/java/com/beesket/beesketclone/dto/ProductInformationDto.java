92a947cf90f84faf05f038e7fd29414359d269a2
hyeonorof <hyeonorof@gmail.com>
2022-08-04 17:04:41 +0900

    ProductInformationDto 주석 추가

--- src/main/java/com/beesket/beesketclone/dto/ProductInformationDto.java
+++ src/main/java/com/beesket/beesketclone/dto/ProductInformationDto.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Getter
 public class ProductInformationDto {
     private Long id;
-    private List<String> imageList;
-    private String productName;
-    private int price;
+    private List<String> imageList; //이미지 리스트
+    private String productName; //제품 내용
+    private int price; //제품 가격
 
     public ProductInformationDto(Product product, List<String> imageDtoList) {
         this.id = product.getId();
