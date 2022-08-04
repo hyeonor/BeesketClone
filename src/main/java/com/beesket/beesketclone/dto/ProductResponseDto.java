ece2ddcd989f3c385b2beac66664f317437af58d
hyeonorof <hyeonorof@gmail.com>
2022-08-04 17:05:09 +0900

    ProductResponseDto 주석 추가

--- src/main/java/com/beesket/beesketclone/dto/ProductResponseDto.java
+++ src/main/java/com/beesket/beesketclone/dto/ProductResponseDto.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Setter
 @Getter
 public class ProductResponseDto {
-    private List<ProductInformationDto> productInfo;
-    private int totalCount;
+    private List<ProductInformationDto> productInfo; //제품 정보
+    private int totalCount; // 카테고리 별 제품 전체 개수
 }
