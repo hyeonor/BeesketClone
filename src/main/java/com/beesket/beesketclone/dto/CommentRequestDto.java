69712fb7b8a0132daac97615f2e4cc1bebec06da
hyeonorof <hyeonorof@gmail.com>
2022-08-04 16:59:26 +0900

    CommentRequestDto 주석 추가

--- src/main/java/com/beesket/beesketclone/dto/CommentRequestDto.java
+++ src/main/java/com/beesket/beesketclone/dto/CommentRequestDto.java
@@ -10,6 +10,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CommentRequestDto {
-    private int scope;
-    private String content;
+    private int scope; //별점
+    private String content; //구매평 내용
 }
